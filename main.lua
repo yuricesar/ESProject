5b27dee97356e563f41e9c6723e307127d762068
Yuri Cesar <yuri.pcesar@gmail.com>
2015-01-20 11:38:11 -0300

    Mudança de variáveis e arquivos

--- main.lua
+++ main.lua
@@ -32,17 +32,17 @@ local score
 
 -- [Worms]
 
-local w1
-local w2
-local w3
-local w4
-local w5
-local w6
-local w7
-local w8
+local m1
+local m2
+local m3
+local m4
+local m5
+local m6
+local m7
+local m8
 
-local worms
-local lastWorm = {}
+local moles
+local lastMole = {}
 
 -- Load Sound
 
@@ -51,9 +51,9 @@ local hit = audio.loadSound('hit.wav')
 -- Variables
 
 local timerSource
-local currentWorms = 0
-local wormsHit = 0
-local totalWorms = 10
+local currentMoles = 0
+local molesHit = 0
+local totalMoles = 10
 
 -- Functions
 
@@ -62,11 +62,11 @@ local startButtonListeners = {}
 local showCredits = {}
 local hideCredits = {}
 local showGameView = {}
-local prepareWorms = {}
+local prepareMoles = {}
 local startTimer = {}
-local showWorm = {}
+local showMole = {}
 local popOut = {}
-local wormHit = {}
+local moleHit = {}
 local alert = {}
 
 -- Main Function
@@ -105,71 +105,71 @@ end
 
 function showGameView:tap(e)
 	transition.to(titleView, {time = 300, x = -titleView.height, onComplete = function() startButtonListeners('rmv') display.remove(titleView) titleView = nil end})
-	score = display.newText('0' .. '/' .. totalWorms, 58, 6, native.systemFontBold, 16)
+	score = display.newText('0' .. '/' .. totalMoles, 58, 6, native.systemFontBold, 16)
 	score:setTextColor(238, 238, 238)
-	prepareWorms()
+	prepareMoles()
 end
 
 function prepareWorms()
-	w1 = display.newImage('worm.png', 80.5, 11)
-	w2 = display.newImage('worm.png', 198.5, 51)
-	w3 = display.newImage('worm.png', 338.5, 34)
-	w4 = display.newImage('worm.png', 70.5, 110)
-	w5 = display.newImage('worm.png', 225.5, 136)
-	w6 = display.newImage('worm.png', 376.5, 96)
-	w7 = display.newImage('worm.png', 142.5, 211)
-	w8 = display.newImage('worm.png', 356.5, 186)
+	m1 = display.newImage('mole.png', 80.5, 11)
+	m2 = display.newImage('mole.png', 198.5, 51)
+	m3 = display.newImage('mole.png', 338.5, 34)
+	m4 = display.newImage('mole.png', 70.5, 110)
+	m5 = display.newImage('mole.png', 225.5, 136)
+	m6 = display.newImage('mole.png', 376.5, 96)
+	m7 = display.newImage('mole.png', 142.5, 211)
+	m8 = display.newImage('mole.png', 356.5, 186)
 	
-	worms = display.newGroup(w1, w2, w3, w4, w5, w6, w7, w8)
+	moles = display.newGroup(m1, m2, m3, m4, m5, m6, m7, m8)
 	
-	for i = 1, worms.numChildren do
-		worms[i]:addEventListener('tap', wormHit)
-		worms[i].isVisible = false
+	for i = 1, moles.numChildren do
+		moles[i]:addEventListener('tap', moleHit)
+		moles[i].isVisible = false
 	end
 	
 	startTimer()
 end
 
 function startTimer()
-	timerSource = timer.performWithDelay(1400, showWorm, 0)
+	timerSource = timer.performWithDelay(1400, showMole, 0)
 end
 
-function showWorm(e)
-	if(currentWorms == totalWorms) then
+function showMole(e)
+	if(currentMoles == totalMoles) then
 		alert()
 	else
-		lastWorm.isVisible = false
+		lastMole.isVisible = false
 		local randomHole = math.floor(math.random() * 8) + 1
 		
-		lastWorm = worms[randomHole]
-		lastWorm:setReferencePoint(display.BottomCenterReferencePoint)
-		lastWorm.yScale = 0.1
-		lastWorm.isVisible = true
+		lastMole = moles[randomHole]
+		lastMole:setReferencePoint(display.BottomCenterReferencePoint)
+		lastMole.yScale = 0.1
+		lastMole.isVisible = true
 		
 		Runtime:addEventListener('enterFrame', popOut)
 		
-		currentWorms = currentWorms + 1
+		currentMoles = currentMoles + 1
 	end
 end
 
 function popOut(e)
-	lastWorm.yScale = lastWorm.yScale + 0.2
+	lastMole.yScale = lastMole.yScale + 0.2
 	
-	if(lastWorm.yScale >= 1) then
+	if(lastMole.yScale >= 1) then
 		Runtime:removeEventListener('enterFrame', popOut)
 	end
 end
 
-function wormHit:tap(e)
+function moleHit:tap(e)
 	audio.play(hit)
-	wormsHit = wormsHit + 1
-	score.text = wormsHit .. '/' .. totalWorms
-	lastWorm.isVisible = false
+	molesHit = molesHit + 1
+	score.text = molesHit .. '/' .. totalMoles
+	lastMole.isVisible = false
 end
 
 function alert()
 	timer.cancel(timerSource)
-	lastWorm.isVisible = false
+	lastMole.isVisible = false
 	
 	local alert = display.newImage('alertBg.png')
 	alert:setReferencePoint(display.CenterReferencePoint)
@@ -177,7 +177,7 @@ function alert()
 	alert.y = display.contentCenterY
 	transition.from(alert, {time = 300, xScale = 0.3, yScale = 0.3})
 	
-	local score = display.newText(wormsHit .. '/' .. totalWorms, 220, 190, native.systemFontBold, 20)
+	local score = display.newText(molesHit .. '/' .. totalMoles, 220, 190, native.systemFontBold, 20)
 	score:setTextColor(204, 152, 102)
 end
 
